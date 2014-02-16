0665c6dc5c02e41e521aecf609bceee8395ec88e
Benjamin T Jones <btjones16@gmail.com>
2014-02-15 23:47:30 -0500

    Added data models

--- ksl/core/models.py
+++ ksl/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    text = models.CharField(max_length=128)
+    pub_date = models.DateTimeField('date published')
+    date_updated = models.DateTimeField('date updated')
+
+class Keyword(models.Model):
+    text = models.CharField(max_length=128)
+    date_updated = models.DateTimeField('date updated')
+
+class WordToKeyword(models.Model):
+    word = models.ForeignKey(Word)
+    keyword = models.ForeignKey(Keyword)
+    date_updated = models.DateTimeField('date updated')
+
+class Video(models.Model):
+    filename = models.CharField(max_length=512)
+    pub_date = models.DateTimeField('date published')
+    date_updated = models.DateTimeField('date updated')
+
+class WordToVideo(models.Model):
+    word = models.ForeignKey(Word)
+    video = models.ForeignKey(Video)
+    date_updated = models.DateTimeField('date updated')
